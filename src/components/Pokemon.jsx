bae01d9cc8a5a9b6da326f2e79bd2f6732fb0035
Aditya <85357798+Adion52330@users.noreply.github.com>
2022-01-07 18:12:34 +0530

    Update Pokemon.jsx

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import "./Pokemon.css";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 
 const style = {
